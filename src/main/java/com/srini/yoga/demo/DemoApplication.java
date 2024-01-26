114810398425618cf7fb83ff8711b17d13cc1fa2
Yogananda <yogananda.chandramouli@necam.com>
2024-01-26 11:43:25 -0800

    Added the Dockerfile for testing

--- src/main/java/com/srini/yoga/demo/DemoApplication.java
+++ src/main/java/com/srini/yoga/demo/DemoApplication.java
@@ -11,7 +11,7 @@ public class DemoApplication {
 
     @GetMapping("/hello")
     public String message() {
-        return "Welcome to minikube Hello world example";
+        return "Welcome to Minikube Hello world example";
     }
 
     public static void main(String[] args) {
