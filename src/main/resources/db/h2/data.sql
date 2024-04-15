291e95712dfbe2f52934cd17dfba3e78e33bb39c
Yogananda <yogananda.chandramouli@necam.com>
2024-04-15 15:19:29 -0700

    Added the sample schema files.

---
+++ src/main/resources/db/h2/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO UserT VALUES (default,1, 'admin', 'admin',25, 'ROLE@ADMIN');
+INSERT INTO UserT VALUES (default,2, 'user', 'user',22, 'ROLE@USER');
+
+INSERT INTO Book VALUES (default,'Spring');
+INSERT INTO Book VALUES (default,'Java');
