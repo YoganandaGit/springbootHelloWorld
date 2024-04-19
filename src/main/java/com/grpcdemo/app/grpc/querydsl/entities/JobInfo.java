c57f1cd7484ae2b629ca7f1e9786a778ba4023df
Yogananda <yogananda.chandramouli@necam.com>
2024-04-19 16:35:01 -0700

    Code cleanup.

--- src/main/java/com/grpcdemo/app/grpc/querydsl/entities/JobInfo.java
+++ src/main/java/com/grpcdemo/app/grpc/querydsl/entities/JobInfo.java
@@ -3,8 +3,10 @@ package com.grpcdemo.app.grpc.querydsl.entities;
 import com.grpcdemo.app.grpc.querydsl.base.BaseEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
@@ -14,6 +16,8 @@ import lombok.ToString;
 @Setter
 @ToString
 @EqualsAndHashCode(callSuper = false)
+@AllArgsConstructor
+@NoArgsConstructor
 public class JobInfo extends BaseEntity {
     private String jobId;
     private String jobRef;
--- src/main/java/com/grpcdemo/app/grpc/service/ExternalApiService.java
+++ src/main/java/com/grpcdemo/app/grpc/service/ExternalApiService.java
@@ -22,18 +22,18 @@ public class ExternalApiService {
     }
 
     public JobQueueMessageProto.JobInfoResponse getJobInfo(String jobId) {
-        //Get all the JobInfos and print title
-        JobInfo jobInfo = saveJobInfo(jobId);
-        //Print all job information
-        IterableUtils.emptyIfNull(getJobInfos()).forEach(jobInfo1 -> log.info("JobInfo: " + jobInfo1));
+        //Save job information
+        JobInfo jobInfo = saveJobInfo(jobId, "JobRef_" + jobId, "OtherRef_" + jobId, "OtherRef2_" + jobId, "JobExternalRef_" + jobId);
         JobInfo jobInfoByJobId = IterableUtils.first(getJobInfos());
         JobQueueMessageProto.JobInfoResponse.Builder jobInfoResponseBuilder = JobQueueMessageProto.JobInfoResponse.newBuilder();
         jobInfoResponseBuilder.setJobId(jobId).setJobRef(jobInfoByJobId.getJobRef()).setJobExternalRef(jobInfo.getJobExternalRef()).setOtherRef1(jobInfo.getOtherRef1()).setOtherRef2(jobInfo.getOtherRef2()).build();
-        deleteJobInfo(NumberUtils.createLong(String.valueOf(jobInfoByJobId.getId())));
         return jobInfoResponseBuilder.build();
     }
 
     public JobQueueMessageProto.JobInfoResponses getJobInfoList() {
+        //Print all job information
+        IterableUtils.emptyIfNull(getJobInfos()).forEach(jobInfo1 -> log.info("JobInfo: " + jobInfo1));
+        //Retrieve all job information and return
         JobQueueMessageProto.JobInfoResponses.Builder jobInfoResponsesBuilder = JobQueueMessageProto.JobInfoResponses.newBuilder();
         IterableUtils.emptyIfNull(getJobInfos()).forEach(jobInfo -> {
             JobQueueMessageProto.JobInfoResponse.Builder jobInfoResponseBuilder = JobQueueMessageProto.JobInfoResponse.newBuilder();
@@ -43,14 +43,8 @@ public class ExternalApiService {
         return jobInfoResponsesBuilder.build();
     }
 
-    public JobInfo saveJobInfo(String jobId) {
-        JobInfo jobInfo = new JobInfo();
-        //Set the fields
-        jobInfo.setJobId(jobId);
-        jobInfo.setJobRef("8888");
-        jobInfo.setOtherRef1("9999");
-        jobInfo.setOtherRef2("1010");
-        jobInfo.setJobExternalRef("ExtRef1111");
+    public JobInfo saveJobInfo(String jobId, String jobRef, String otherRef1, String otherRef2, String jobExternalRef) {
+        JobInfo jobInfo = new JobInfo(jobId, jobRef, otherRef1, otherRef2, jobExternalRef);
         jobInfoRepository.save(jobInfo);
         log.info("JobInfo saved successfully");
         return jobInfo;
